df787b95a8fc94481facca611d340774a90aa31a
HongGit <soojam@konkuk.ac.kr>
2024-10-31 01:07:06 +0900

    [FIX/#151] 대학 메일 자체 인증 에러 추가

--- src/main/java/org/sopt/seonyakServer/global/config/FeignConfig.java
+++ src/main/java/org/sopt/seonyakServer/global/config/FeignConfig.java
@@ -1,18 +1,11 @@
 package org.sopt.seonyakServer.global.config;
 
-import feign.codec.ErrorDecoder;
 import org.sopt.seonyakServer.SeonyakServerApplication;
-import org.sopt.seonyakServer.global.common.external.univcert.feign.FeignErrorDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackageClasses = SeonyakServerApplication.class)
 public class FeignConfig {
 
-    @Bean
-    public ErrorDecoder errorDecoder() {
-        return new FeignErrorDecoder();
-    }
 }
