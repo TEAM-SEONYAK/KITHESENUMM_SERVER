f9c80636b885b1548f6ebc4607411df5919e3505
HongGit <soojam@konkuk.ac.kr>
2024-10-31 01:04:38 +0900

    [DEL/#151] 기존 대학 인증 방식 제거

--- src/main/java/org/sopt/seonyakServer/global/common/external/univcert/dto/UnivCertVerifyRequest.java
+++
@@ -1,8 +0,0 @@
-package org.sopt.seonyakServer.global.common.external.univcert.dto;
-
-public record UnivCertVerifyRequest(
-        String email,
-        String univName,
-        int code
-) {
-}
