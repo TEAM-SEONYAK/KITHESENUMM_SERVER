98f2d80ea9616902653458759009f37faccb6673
ckkim817 <ckkim817@konkuk.ac.kr>
2024-12-26 02:49:20 +0900

    [FEAT/#168] 토큰 만료 여부 확인 API 구현

--- src/main/java/org/sopt/seonyakServer/domain/senior/controller/SeniorController.java
+++ src/main/java/org/sopt/seonyakServer/domain/senior/controller/SeniorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.seonyakServer.domain.senior.dto.SeniorCardProfileResponse;
 import org.sopt.seonyakServer.domain.senior.dto.SeniorFilterResponse;
+import org.sopt.seonyakServer.domain.senior.dto.SeniorNameResponse;
 import org.sopt.seonyakServer.domain.senior.dto.SeniorProfileRequest;
 import org.sopt.seonyakServer.domain.senior.dto.SeniorProfileResponse;
 import org.sopt.seonyakServer.domain.senior.model.PreferredTimeList;
@@ -19,12 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/senior")
+@RequestMapping("/api/v1")
 public class SeniorController {
 
     private final SeniorService seniorService;
 
-    @PatchMapping("/profile")
+    @PatchMapping("/senior/profile")
     public ResponseEntity<Void> patchProfile(
             @RequestBody SeniorProfileRequest seniorProfileRequest
     ) {
@@ -32,14 +33,14 @@ public class SeniorController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/time/{seniorId}")
+    @GetMapping("/senior/time/{seniorId}")
     public ResponseEntity<PreferredTimeList> getPreferredTime(
             @PathVariable final Long seniorId
     ) {
         return ResponseEntity.ok(seniorService.getSeniorPreferredTime(seniorId));
     }
 
-    @GetMapping("/search")
+    @GetMapping("/senior/search")
     public ResponseEntity<SeniorFilterResponse> searchFieldPosition(
             @RequestParam(required = false) final List<String> field,
             @RequestParam(required = false) final List<String> position
@@ -47,17 +48,22 @@ public class SeniorController {
         return ResponseEntity.ok(seniorService.searchSeniorFieldPosition(field, position));
     }
 
-    @GetMapping("/{seniorId}")
+    @GetMapping("/senior/{seniorId}")
     public ResponseEntity<SeniorProfileResponse> getSeniorProfile(
             @PathVariable final Long seniorId
     ) {
         return ResponseEntity.ok(seniorService.getSeniorProfile(seniorId));
     }
 
-    @GetMapping("/card/{seniorId}")
+    @GetMapping("/senior/card/{seniorId}")
     public ResponseEntity<SeniorCardProfileResponse> getSeniorCardProfile(
             @PathVariable final Long seniorId
     ) {
         return ResponseEntity.ok(seniorService.getSeniorCardProfile(seniorId));
+    }
+
+    @GetMapping("/auth/token/validate")
+    public ResponseEntity<SeniorNameResponse> validTokenExpired() {
+        return ResponseEntity.ok(seniorService.validTokenExpired());
     }
 }
